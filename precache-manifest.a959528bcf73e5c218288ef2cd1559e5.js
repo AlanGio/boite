1a7f9534b2377839778d29570502ae8fede1851f
Alan <alan.gio@gmail.com>
2023-07-25 20:09:17 -0300

    Updates

---
+++ precache-manifest.a959528bcf73e5c218288ef2cd1559e5.js
@@ -0,0 +1,330 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a127754fe4fbcaa422ce98025804e55b",
+    "url": "/boite/index.html"
+  },
+  {
+    "revision": "472d0c180dad36985594",
+    "url": "/boite/static/css/2.f608af5e.chunk.css"
+  },
+  {
+    "revision": "0a0351016664d5d9f527",
+    "url": "/boite/static/css/main.5ac202d0.chunk.css"
+  },
+  {
+    "revision": "472d0c180dad36985594",
+    "url": "/boite/static/js/2.01925e4c.chunk.js"
+  },
+  {
+    "revision": "0a0351016664d5d9f527",
+    "url": "/boite/static/js/main.0d960c08.chunk.js"
+  },
+  {
+    "revision": "30afb9d167345ad47cf4",
+    "url": "/boite/static/js/runtime~main.85be71a0.js"
+  },
+  {
+    "revision": "567dc8e61a8be6e5044f2b04b58485fc",
+    "url": "/boite/static/media/8.567dc8e6.jpg"
+  },
+  {
+    "revision": "80ec9b6b78cab30a860a52a2ac9a4817",
+    "url": "/boite/static/media/AdobeStock_163058165.80ec9b6b.jpeg"
+  },
+  {
+    "revision": "408925831f22e5347d757d7d913cc224",
+    "url": "/boite/static/media/AdobeStock_187335069.40892583.jpg"
+  },
+  {
+    "revision": "92205f251ba4499a0be2faaf82d37d7b",
+    "url": "/boite/static/media/AdobeStock_187335069.92205f25.jpeg"
+  },
+  {
+    "revision": "05ac43c793c6ba7ff5ba5082eba4f14b",
+    "url": "/boite/static/media/AdobeStock_190349796_Preview.05ac43c7.jpeg"
+  },
+  {
+    "revision": "4e5459f1ae437a984964a07c792a47c3",
+    "url": "/boite/static/media/AdobeStock_203084491.4e5459f1.jpg"
+  },
+  {
+    "revision": "bb0387122760972c6c44e3e5921d7db0",
+    "url": "/boite/static/media/AdobeStock_203084491_Preview.bb038712.jpeg"
+  },
+  {
+    "revision": "e2953862317ec6cef8124aa1ed37e2a7",
+    "url": "/boite/static/media/AdobeStock_229230610_Preview.e2953862.jpeg"
+  },
+  {
+    "revision": "a34b23fd3478d98094878f8fa50037dd",
+    "url": "/boite/static/media/AdobeStock_259547347.a34b23fd.jpg"
+  },
+  {
+    "revision": "c816c0d7046e31e5177f60da2feeb031",
+    "url": "/boite/static/media/AdobeStock_261159405_Preview.c816c0d7.jpeg"
+  },
+  {
+    "revision": "272f8982cc4109b5eb584d5039c21a4b",
+    "url": "/boite/static/media/AdobeStock_263790084.272f8982.png"
+  },
+  {
+    "revision": "ba2fb1a106da8fcc89d4862880c75c68",
+    "url": "/boite/static/media/AdobeStock_263790084.ba2fb1a1.jpg"
+  },
+  {
+    "revision": "5de47e3b464c2af90d3adc4c954c7cc8",
+    "url": "/boite/static/media/AdobeStock_282970391_Preview.5de47e3b.jpeg"
+  },
+  {
+    "revision": "967d5ecaeeba08cb6c7a834222f89034",
+    "url": "/boite/static/media/AdobeStock_284333412_Preview.967d5eca.jpeg"
+  },
+  {
+    "revision": "a660cf198dfb68b9881731a695171118",
+    "url": "/boite/static/media/about.a660cf19.jpg"
+  },
+  {
+    "revision": "38a3ca16d365a111aaa7babfa331a073",
+    "url": "/boite/static/media/cabello.38a3ca16.svg"
+  },
+  {
+    "revision": "441b0a174cc0c4e7f039c438217e44d7",
+    "url": "/boite/static/media/cancel_anytime.441b0a17.svg"
+  },
+  {
+    "revision": "d770db73ebbd68ae17e410c206c0eef1",
+    "url": "/boite/static/media/casual.d770db73.jpg"
+  },
+  {
+    "revision": "54ee8b46cd1b5b308e7f2307ffbdd238",
+    "url": "/boite/static/media/cejas.54ee8b46.svg"
+  },
+  {
+    "revision": "a78df07f80668cabca498a076d0c9d43",
+    "url": "/boite/static/media/citricas.a78df07f.jpg"
+  },
+  {
+    "revision": "00200ac4d6ba9af010f305ba017c9f4e",
+    "url": "/boite/static/media/customized_box.00200ac4.svg"
+  },
+  {
+    "revision": "820cb7cdf7e1780c665d9605e1e54f79",
+    "url": "/boite/static/media/elegante.820cb7cd.jpg"
+  },
+  {
+    "revision": "287ec3f7fd0dba85717c0953bfe26c29",
+    "url": "/boite/static/media/eventos_sociales.287ec3f7.svg"
+  },
+  {
+    "revision": "38a47eb9c990c3244f3a4b7dd230f407",
+    "url": "/boite/static/media/eventos_sociales.38a47eb9.jpg"
+  },
+  {
+    "revision": "2925ad7ee3af6f9badd7ced2e4cb8a04",
+    "url": "/boite/static/media/fiesta.2925ad7e.svg"
+  },
+  {
+    "revision": "1ea2bae82934ef20d4fe05e20459cdc8",
+    "url": "/boite/static/media/florales.1ea2bae8.jpg"
+  },
+  {
+    "revision": "ec3a3b56e7f0669f730611716ed6fa5e",
+    "url": "/boite/static/media/free_shipping.ec3a3b56.svg"
+  },
+  {
+    "revision": "be492ceb5b3e824f58d5b2039b966f09",
+    "url": "/boite/static/media/frescas.be492ceb.jpg"
+  },
+  {
+    "revision": "c6329409a0df76cc8e41b6a34e618a3e",
+    "url": "/boite/static/media/glam.c6329409.jpg"
+  },
+  {
+    "revision": "c3184e9bb6c688c484dc52e1126cd132",
+    "url": "/boite/static/media/graso.c3184e9b.jpg"
+  },
+  {
+    "revision": "dc6135161a1a3b3d48eb44cbaadf0b52",
+    "url": "/boite/static/media/hebillas.dc613516.jpg"
+  },
+  {
+    "revision": "33bbee2354461b367e8fba7dc15f4367",
+    "url": "/boite/static/media/liso.33bbee23.jpg"
+  },
+  {
+    "revision": "44294568f3128e419454dbc0b95918a6",
+    "url": "/boite/static/media/logo.44294568.svg"
+  },
+  {
+    "revision": "d9b4a964cf83a22a79b9ea9b8ef77c46",
+    "url": "/boite/static/media/maderas.d9b4a964.jpg"
+  },
+  {
+    "revision": "b0b54b7b3965e77c59d447c3f7f02a5f",
+    "url": "/boite/static/media/manos.b0b54b7b.svg"
+  },
+  {
+    "revision": "1247056b321e46eb1238530fe6310d8e",
+    "url": "/boite/static/media/ojos.1247056b.svg"
+  },
+  {
+    "revision": "8ed582b974e91a59748ddd6dea170162",
+    "url": "/boite/static/media/ondulado.8ed582b9.jpg"
+  },
+  {
+    "revision": "3d5fbdeb4a299a37bca1ca1121a917ea",
+    "url": "/boite/static/media/pelo.3d5fbdeb.svg"
+  },
+  {
+    "revision": "f8cdd58502b823905615d2d009fc1215",
+    "url": "/boite/static/media/piel.f8cdd585.svg"
+  },
+  {
+    "revision": "b43ca79e8e544a4e99dec9febd27b741",
+    "url": "/boite/static/media/pomulos.b43ca79e.svg"
+  },
+  {
+    "revision": "479cc4b498341db83be76e324c019088",
+    "url": "/boite/static/media/prendedores.479cc4b4.jpg"
+  },
+  {
+    "revision": "423f6d0633af9ec67019c622a5964ca8",
+    "url": "/boite/static/media/pulseras.423f6d06.jpg"
+  },
+  {
+    "revision": "9377566ed5e29ae398207cbdcf484f2e",
+    "url": "/boite/static/media/rizado.9377566e.jpg"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/boite/static/media/roboto-latin-100.5cb7edfc.woff"
+  },
+  {
+    "revision": "7370c3679472e9560965ff48a4399d0b",
+    "url": "/boite/static/media/roboto-latin-100.7370c367.woff2"
+  },
+  {
+    "revision": "f8b1df51ba843179fa1cc9b53d58127a",
+    "url": "/boite/static/media/roboto-latin-100italic.f8b1df51.woff2"
+  },
+  {
+    "revision": "f9e8e590b4e0f1ff83469bb2a55b8488",
+    "url": "/boite/static/media/roboto-latin-100italic.f9e8e590.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/boite/static/media/roboto-latin-300.b00849e0.woff"
+  },
+  {
+    "revision": "ef7c6637c68f269a882e73bcb57a7f6a",
+    "url": "/boite/static/media/roboto-latin-300.ef7c6637.woff2"
+  },
+  {
+    "revision": "14286f3ba79c6627433572dfa925202e",
+    "url": "/boite/static/media/roboto-latin-300italic.14286f3b.woff2"
+  },
+  {
+    "revision": "4df32891a5f2f98a363314f595482e08",
+    "url": "/boite/static/media/roboto-latin-300italic.4df32891.woff"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/boite/static/media/roboto-latin-400.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/boite/static/media/roboto-latin-400.60fa3c06.woff"
+  },
+  {
+    "revision": "51521a2a8da71e50d871ac6fd2187e87",
+    "url": "/boite/static/media/roboto-latin-400italic.51521a2a.woff2"
+  },
+  {
+    "revision": "fe65b8335ee19dd944289f9ed3178c78",
+    "url": "/boite/static/media/roboto-latin-400italic.fe65b833.woff"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/boite/static/media/roboto-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/boite/static/media/roboto-latin-500.87284894.woff"
+  },
+  {
+    "revision": "288ad9c6e8b43cf02443a1f499bdf67e",
+    "url": "/boite/static/media/roboto-latin-500italic.288ad9c6.woff"
+  },
+  {
+    "revision": "db4a2a231f52e497c0191e8966b0ee58",
+    "url": "/boite/static/media/roboto-latin-500italic.db4a2a23.woff2"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/boite/static/media/roboto-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/boite/static/media/roboto-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "81f57861ed4ac74741f5671e1dff2fd9",
+    "url": "/boite/static/media/roboto-latin-700italic.81f57861.woff"
+  },
+  {
+    "revision": "da0e717829e033a69dec97f1e155ae42",
+    "url": "/boite/static/media/roboto-latin-700italic.da0e7178.woff2"
+  },
+  {
+    "revision": "9b3766ef4a402ad3fdeef7501a456512",
+    "url": "/boite/static/media/roboto-latin-900.9b3766ef.woff2"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/boite/static/media/roboto-latin-900.bb1e4dc6.woff"
+  },
+  {
+    "revision": "28f9151055c950874d2c6803a39b425b",
+    "url": "/boite/static/media/roboto-latin-900italic.28f91510.woff"
+  },
+  {
+    "revision": "ebf6d1640ccddb99fb49f73c052c55a8",
+    "url": "/boite/static/media/roboto-latin-900italic.ebf6d164.woff2"
+  },
+  {
+    "revision": "961b47d016db4727ba3cf182eefc4608",
+    "url": "/boite/static/media/seca.961b47d0.jpg"
+  },
+  {
+    "revision": "92b22c12f0cb14e42bf860788b6184a1",
+    "url": "/boite/static/media/sin_volumen.92b22c12.jpg"
+  },
+  {
+    "revision": "27e7cd12b85b28b58bfe8f40c3f1c1a9",
+    "url": "/boite/static/media/sorteo.27e7cd12.jpeg"
+  },
+  {
+    "revision": "3d495030882e60823fd1fac457191ca2",
+    "url": "/boite/static/media/step1.3d495030.jpg"
+  },
+  {
+    "revision": "3cc6868dd683c22472735a8eb90adeb0",
+    "url": "/boite/static/media/step2.3cc6868d.jpg"
+  },
+  {
+    "revision": "41c37118ff7df38c3e65173c297d244c",
+    "url": "/boite/static/media/step3.41c37118.jpg"
+  },
+  {
+    "revision": "0354062d84667ba39cbde7c2fa92e882",
+    "url": "/boite/static/media/trabajo.0354062d.svg"
+  },
+  {
+    "revision": "3eab403018914c58ba83c2ca5f1c8063",
+    "url": "/boite/static/media/urbano.3eab4030.jpg"
+  },
+  {
+    "revision": "41596b4bd279317c80014d803f44b42c",
+    "url": "/boite/static/media/why.41596b4b.jpg"
+  }
+]);
